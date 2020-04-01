6529b37b7a406a51dc471555f8335a1b246e94d4
hoang97 <50277561+hoang97@users.noreply.github.com>
2020-04-01 19:13:01 +0700

    Add files via upload

--- lab1/billing.py
+++ lab1/billing.py
@@ -5,14 +5,19 @@ def invoices(phoneNum):
     dest = fiels.index('msisdn_dest')
     call = fiels.index('call_duration')
     sms = fiels.index('sms_number')
+    call_out_coef = 1
+    call_in_coef = 1
     call_free = 0
-    sms_free = 10
-    call_duration = 0
+    sms_free = 5
+    call_out_duration = 0
+    call_in_duration = 0
     sms_number = 0
     for row in data:
         if row[origin] == phoneNum:
-            call_duration += float(row[call])
+            call_out_duration += float(row[call])
             sms_number += float(row[sms])
-    call_invoices = max(0,(call_duration-call_free)*2)
-    sms_invoices = max(0,(sms_number-sms_free)*1)
+        if row[dest] == phoneNum:
+            call_in_duration += float(row[call])
+    call_invoices = max(0,(call_out_duration-call_free)*call_out_coef+call_in_duration*call_in_coef)
+    sms_invoices = max(0,(sms_number-sms_free)*1) + max(0,(sms_number-10)*1)
     return call_invoices,sms_invoices
